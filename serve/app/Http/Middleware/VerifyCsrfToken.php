5a36f3203599a01f0e83bf779224905e66460430
pijunkuan <pijunkuan@gmail.com>
2020-05-20 05:29:29 +0800

    Update VerifyCsrfToken.php

--- serve/app/Http/Middleware/VerifyCsrfToken.php
+++ serve/app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        "/shop/pay/confirm"
+        "/shop/pay/confirm",
+        "/front/pay/confirm",
     ];
 }
