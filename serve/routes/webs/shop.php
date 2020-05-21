bc80e75d8ed9314bab6895dc07170982023486f6
pijunkuan <pijunkuan@gmail.com>
2020-05-21 17:26:31 +0800

    Update shop.php

--- serve/routes/webs/shop.php
+++ serve/routes/webs/shop.php
@@ -3,4 +3,5 @@
 Route::get("","Web\WebController@web");
 Route::get('checksuc',"Web\WebController@web");
 Route::get("order/pay/{no}","Front\Pay\PayController@pay")->name('front.order.pay');
+Route::get('{all}',"Web\WebController@web");
 
