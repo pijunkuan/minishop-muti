992782a1b22410f8117df6565f99385a71fc4d52
pijunkuan <pijunkuan@gmail.com>
2020-05-31 20:45:43 +0800

    Update OrderCancelListener.php

--- serve/app/Listeners/Order/Status/OrderCancelListener.php
+++ serve/app/Listeners/Order/Status/OrderCancelListener.php
@@ -33,7 +33,7 @@ class OrderCancelListener
      */
     public function handle(OrderStatusEvent $event)
     {
-        if ($event->status == Order::ORDER_STATUS_PENDING) {
+        if ($event->status == Order::ORDER_STATUS_CANCEL) {
             $order = $event->order;
             if ($order['status'] != Order::ORDER_STATUS_PENDING) throw (new HttpResponseException(response()->json(['code' => 422, "message" => "该订单状态下无法取消订单", "data" => null,], 422)));
             DB::beginTransaction();
