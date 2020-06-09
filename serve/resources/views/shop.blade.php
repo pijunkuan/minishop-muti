61b521b10a5d1f7e5671259d467eef0242ee94e7
pijunkuan <pijunkuan@gmail.com>
2020-06-09 20:16:20 +0800

    Update shop.blade.php

--- serve/resources/views/shop.blade.php
+++ serve/resources/views/shop.blade.php
@@ -6,14 +6,14 @@
         <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
         <link rel=stylesheet href="//at.alicdn.com/t/font_1789330_ptlh42r5dqn.css">
         <title>{{$title}}</title>
-        <link href="{{asset($file.'css/template-libs.css')}}" rel=stylesheet>
-        <link href="{{asset($file.'css/app.css')}}" rel=stylesheet>
+        <link href="{{asset($file.'css/template-libs.css?v='.time())}}" rel=stylesheet>
+        <link href="{{asset($file.'css/app.css?v='.time())}}" rel=stylesheet>
 
     </head>
     <body>
        <div id="app"></div>
-       <script src="{{asset($file.'js/template-libs.js')}}"></script>
-       <script src="{{asset($file.'js/app.js')}}"></script>
+       <script src="{{asset($file.'js/template-libs.js?v='.time())}}"></script>
+       <script src="{{asset($file.'js/app.js?v='.time())}}"></script>
     </body>
 
 
